6376695792fcd045eb2efbaa943cec7ad84190c1
Tech Guy One <166887202+Tech1Guy1One@users.noreply.github.com>
2024-04-15 22:06:58 -0400

    Update 11. (Optional)-WindowsUpdateDisabled.bat
    Adds the required changes in order for the update package to run and stop correctly on Ghost and Xlite 10 systems

--- 11. (Optional)-WindowsUpdateDisabled.bat
+++ 11. (Optional)-WindowsUpdateDisabled.bat
@@ -2,7 +2,23 @@
 sc config wuauserv start= disabled
 sc config bits start= disabled
 sc config usosvc start= disabled
+sc config EventLog start= disabled
+sc config LicenseManager start= disabled
+sc config WpnService start= disabled
+sc config edgeupdate start= disabled
+sc config edgeupdatem start= disabled
 net stop wuauserv
 net stop bits
 net stop usosvc
+net stop edgeupdatem
+net stop edgeupdate
+net stop WpnService
+net stop LicenseManager
+net stop EventLog
+net stop TokenBroker
+net stop Schedule
+net stop SENS
+net stop NcbService
+net stop CDPSvc
+net stop DiagTrack
 exit
