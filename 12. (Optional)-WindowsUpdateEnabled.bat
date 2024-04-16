770585ebc83eee2916c4fcad1723c3970b24c9e7
Tech Guy One <166887202+Tech1Guy1One@users.noreply.github.com>
2024-04-15 22:06:09 -0400

    Update 12. (Optional)-WindowsUpdateEnabled.bat
    Updated the configuration to allow this to work correctly on the XLite and Ghost system after the lockdown of the data and details

--- 12. (Optional)-WindowsUpdateEnabled.bat
+++ 12. (Optional)-WindowsUpdateEnabled.bat
@@ -2,4 +2,9 @@
 sc config wuauserv start= demand
 sc config bits start= demand
 sc config usosvc start= demand
+sc config EventLog start= demand
+sc config LicenseManager start= demand
+sc config WpnService start= demand
+sc config edgeupdate start= demand
+sc config edgeupdatem start= demand
 exit
