71a097869f72a916457493de2652aa557700be63
Tech Guy One <166887202+Tech1Guy1One@users.noreply.github.com>
2024-05-01 00:19:11 -0400

    Update 7. XLite-10-ServiceFix.bat

--- 7. XLite-10-ServiceFix.bat
+++ 7. XLite-10-ServiceFix.bat
@@ -8,8 +8,14 @@ sc config CDPSvc start= disabled
 sc config DiagTrack start= disabled
 # NCBService or Network connection Broker is a catch 22 service. While it does provide network connection data in the task bar, it also provides microsoft with the telemetry service
 # details and location service details, even when location services are disable. Disabling this service will require all network connection settings be controlled in control panel. 
+# sc config NcbService start= demand
 sc config NcbService start= disabled
+# SENS or System Event Notification Services is used to communicate LAN and Application details to remote locations. 
+# This process is used to transmit data that contains application user details to 3rd party application developers. 
+# Disabling this service may cause issues with TCP quality related alerts in the event viewer, it may also cause issues with QOC details and data. 
+# sc config SENS start= demand 
 sc config SENS start= disabled
+
 sc config Schedule start= disabled
 sc config TokenBroker start= disabled
 sc config EventLog start= disabled
