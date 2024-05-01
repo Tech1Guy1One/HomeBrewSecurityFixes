08f0eb1bf22edeed0ef7253498754ee0c6017438
Tech Guy One <166887202+Tech1Guy1One@users.noreply.github.com>
2024-05-01 00:32:32 -0400

    Update 7. XLite-10-ServiceFix.bat

--- 7. XLite-10-ServiceFix.bat
+++ 7. XLite-10-ServiceFix.bat
@@ -1,3 +1,7 @@
+# Included in this script is a description as to what each of these services do, keep in mind that while I try to provide some details as to what each thing can break, there's additional customizations
+# on each system that I am unable to know for sure. As such, please use this script as a baseline, and modify what you want, or revert what you want. 
+
+
 @echo off
 # Disable Printers, Bluetooth, Scanners, mobile phones, music players, cameras, usb disks, usb attached storage, and XBX Live connections. 
 # This does not disable all connected devices on all systems.. This may or may not cause system issues. 
@@ -15,13 +19,31 @@ sc config NcbService start= disabled
 # Disabling this service may cause issues with TCP quality related alerts in the event viewer, it may also cause issues with QOC details and data. 
 # sc config SENS start= demand 
 sc config SENS start= disabled
-
+# Schedule is the serice associate with task scheduler. This is optional as if you disable this, scheduled tasks will fail to run. 
+# disabling this service can have issues if you have scheduled tasks, they won't run, but this also means that tasks, or background unknown tasks will not run 
+# sc config Schedule start= demand
 sc config Schedule start= disabled
+# Tokenbroker is used for Windows Store, XBox Connections, and Microsoft logon. This service when running allows the hive to connect directly to your machine. 
+# WARNING!!! if you disable TokenBroker and have an online Microsoft account for authentication, you will likely brick your access. 
+# sc config TokenBroker start= demand
 sc config TokenBroker start= disabled
+# Event log is just that, the event log, if you don't need logs, then you can disable this service and save some performance.. Every little bit helps.. 
+# sc config EventLog start= demand
 sc config EventLog start= disabled
+# Once your OS is licensed, the OS will not prompt again to be licensed, this service is not for that, it's for proving legit OS status with the Microsoft Store. 
+# The microsoft Store provides microsoft with Telemetry to microsoft. As such this should be disabled. 
+# sc config LicenseManager start= demand
 sc config LicenseManager start= disabled
+# WPN or windows Push Notifications is basically a new net send application. This process allows 3rd parties to push ads to your systems, they call it content, but it's ads. 
+# Disabling this service will cause issues with getting cheap viagra. This may reduce your Viagra surplus. 
+# sc config WpnService start= demand
 sc config WpnService start= disabled
+# EdgeUpdate as the name suggests, this is the update service for Microsoft Edge. 
+# NOTE, you may not be able to stop the edge update service on vanilla windows, this is a known issue.. 
+# sc config edgeupdate start= demand 
 sc config edgeupdate start= disabled
+# the Edgeupdatem service is a COM service needed to run the EdgeUpdate service itself. Either Both need to be enabled, or both disabled. 
+# sc config edgeupdatem start= demand
 sc config edgeupdatem start= disabled
 net stop edgeupdatem
 net stop edgeupdate
