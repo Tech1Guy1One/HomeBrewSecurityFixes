88994b8556ff66c6371b6b635724d58b513b66df
Tech Guy One <166887202+Tech1Guy1One@users.noreply.github.com>
2024-05-01 00:15:13 -0400

    Update 7. XLite-10-ServiceFix.bat

--- 7. XLite-10-ServiceFix.bat
+++ 7. XLite-10-ServiceFix.bat
@@ -1,6 +1,13 @@
 @echo off
+# Disable Printers, Bluetooth, Scanners, mobile phones, music players, cameras, usb disks, usb attached storage, and XBX Live connections. 
+# This does not disable all connected devices on all systems.. This may or may not cause system issues. 
+# sc config CDPSvc start= demand 
 sc config CDPSvc start= disabled
+# Diagtrack is the service that connects your system to the microsoft hive for data transfer.
+# sc config DiagTrack start= demand
 sc config DiagTrack start= disabled
+# NCBService or Network connection Broker is a catch 22 service. While it does provide network connection data in the task bar, it also provides microsoft with the telemetry service
+# details and location service details, even when location services are disable. Disabling this service will require all network connection settings be controlled in control panel. 
 sc config NcbService start= disabled
 sc config SENS start= disabled
 sc config Schedule start= disabled
